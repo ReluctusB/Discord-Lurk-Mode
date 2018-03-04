e3524d93139507580cbe266a1532b1f13c436f81
ReluctusB <endahallahan@comcast.net>
2018-03-04 01:18:57 -0600

    Reduced DOM interactions, switched to .insertRule

--- DiscordLurkMode.user.js
+++ DiscordLurkMode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DiscordLurkMode
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Adds a lurk mode, which disables Discord's text box. Now 20% more sylish!
 // @author       RB
 // @match        https://discordapp.com/*
@@ -11,18 +11,9 @@
 // @downloadURL  https://raw.githubusercontent.com/ReluctusB/Discord-Lurk-Mode/master/DiscordLurkMode.user.js
 // ==/UserScript==
 
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
-}
-
 function createBox() {
     if (document.getElementsByTagName('textarea')[0]){
+        var frag = document.createDocumentFragment();
         var outDiv = document.createElement("DIV");
         outDiv.className = "lurkDiv";
         var outLabel = document.createElement("LABEL");
@@ -32,21 +23,24 @@ function createBox() {
         var checkBox = document.createElement("input");
         checkBox.type = "checkbox";
         checkBox.id = "checker";
-        addGlobalStyle('.lurkSwitch {position: relative; display: inline-block; width: 25px; height: 15px;}');
-        addGlobalStyle('.lurkSwitch input {display:none;}');
-        addGlobalStyle('.lurkSlider {position: absolute;cursor: pointer;top: 0;left: 0;right: 0;bottom: 0;background-color: #ccc;-webkit-transition: .2s;transition: .2s;}');
-        addGlobalStyle('.lurkSlider:before {position: absolute;content: "";height: 15px; width: 15px;left: 0px;bottom: 0px;background-color: white;-webkit-transition: .2s;transition: .2s;}');
-        addGlobalStyle('input:checked + .lurkSlider {background-color: #8B0000;}');
-        addGlobalStyle('input:focus + .lurkSlider {box-shadow: 0 0 1px #8B0000;}');
-        addGlobalStyle('input:checked + .lurkSlider:before {-webkit-transform: translateX(10px);-ms-transform: translateX(10px);transform: translateX(10px);}');
-        addGlobalStyle('.lurkSlider.round {border-radius: 20px;}');
-        addGlobalStyle('.lurkSlider.round:before {border-radius: 50%;}');
-        addGlobalStyle('.lurkDiv {position:absolute;bottom:17px;left:170px;zIndex:5}');
-        var appFront = document.getElementById('app-mount');
-        appFront.parentNode.insertBefore(outDiv, appFront.nextSibling);
+        var style = document.createElement("style");
+		var sheet = document.head.appendChild(style).sheet;
+        sheet.insertRule('.lurkSwitch {position: relative; display: inline-block; width: 25px; height: 15px;}',sheet.cssRules.length);
+        sheet.insertRule('.lurkSwitch input {display:none;}',sheet.cssRules.length);
+        sheet.insertRule('.lurkSlider {position: absolute;cursor: pointer;top: 0;left: 0;right: 0;bottom: 0;background-color: #ccc;-webkit-transition: .2s;transition: .2s;}',sheet.cssRules.length);
+        sheet.insertRule('.lurkSlider:before {position: absolute;content: "";height: 15px; width: 15px;left: 0px;bottom: 0px;background-color: white;-webkit-transition: .2s;transition: .2s;}',sheet.cssRules.length);
+        sheet.insertRule('input:checked + .lurkSlider {background-color: #8B0000;}',sheet.cssRules.length);
+        sheet.insertRule('input:focus + .lurkSlider {box-shadow: 0 0 1px #8B0000;}',sheet.cssRules.length);
+        sheet.insertRule('input:checked + .lurkSlider:before {-webkit-transform: translateX(10px);-ms-transform: translateX(10px);transform: translateX(10px);}',sheet.cssRules.length);
+        sheet.insertRule('.lurkSlider.round {border-radius: 20px;}',sheet.cssRules.length);
+        sheet.insertRule('.lurkSlider.round:before {border-radius: 50%;}',sheet.cssRules.length);
+        sheet.insertRule('.lurkDiv {position:absolute;bottom:17px;left:170px;zIndex:5}',sheet.cssRules.length);
+        frag.appendChild(outDiv);
         outDiv.appendChild(outLabel);
         outLabel.appendChild(checkBox);
         outLabel.appendChild(outSpan);
+        var appFront = document.getElementById('app-mount');
+        appFront.parentNode.insertBefore(frag, appFront.nextSibling);
     } else {
         setTimeout(createBox,1000);
     }
